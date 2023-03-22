8852af135de3e7b799d85dbf0a4eb6d5d3ca9d00
TARIK <tarikixb@gmail.com>
2023-03-21 18:13:17 -0700

    tarik

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
 
-
-	int main(void)
-	{
+/**
+ * main - main function
+ * 
+ * Return: 0 on success.
+ */
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,5 +16,6 @@
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
-	}
+}
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,18 @@
 #include "main.h"
+#include <stdio.h>
+/**
+* print_alphabet - check the code
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char letter;
 
 
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
-
-
-	void print_alphabet(void)
-	{
-		char letter;
-
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
 
 		_putchar('\n');
-	}
+}
